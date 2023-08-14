34126ba06086981fb7c69aefe0e3b30a005f38d4
lucaz <lucazuccarello14@gmail.com>
2023-08-14 19:19:36 +0200

    Implemented 'addNewElement', 'updateElement' and 'deleteElement'
    also added new constant ' ADD_MENU_URL '

--- frontend/src/app/services/menù.service.ts
+++ frontend/src/app/services/menù.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Kind, Menù } from '../shared/models/menù';
 import { Observable } from 'rxjs';
-import { MENU_URL } from '../shared/constants/urls';
+import { ADD_MENU_URL, MENU_URL } from '../shared/constants/urls';
 import { HttpOptions } from '../shared/models/httpOptions';
 
 @Injectable({
@@ -17,6 +17,17 @@ export class MenùService {
 		return this.http.get<Menù[]>(MENU_URL+'/'+kind,this.httpOptions);
 	}
 
-	
+	addNewElement(name:String, kind: Kind, price: string) : Observable<Menù>{
+		return this.http.post<Menù>(ADD_MENU_URL,{name:name,kind:kind,price:price},this.httpOptions);
+	}
 
+	updateElement(id:String, name:String, kind: Kind, price:String) : Observable<Menù>{
+		return this.http.patch<Menù>(MENU_URL+'/'+id+'/update',{name:name,kind:kind,price:price},this.httpOptions);
+	}
+
+
+	
+	deleteElement(id:String){
+		return this.http.delete<Menù>(MENU_URL+'/'+id+'/delete',this.httpOptions);	
+	}
 }
--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://localhost:443' ;
 
 export const MENU_URL = BASE_URL + '/menu' ;
+export const ADD_MENU_URL = MENU_URL + '/add';
 export const TABLES_URL = BASE_URL + '/tables';
 export const ADD_TABLES_URL = BASE_URL + '/tables/add';
 export const USERS_URL = BASE_URL + '/users' ;

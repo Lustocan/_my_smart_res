ba2aa2652c350673fbb101e387a31234ddd29038
lucaz <lucazuccarello14@gmail.com>
2023-08-15 16:43:46 +0200

    Added popup system in menu function

--- frontend/src/app/services/menù.service.ts
+++ frontend/src/app/services/menù.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Kind, Menù } from '../shared/models/menù';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { ADD_MENU_URL, MENU_URL } from '../shared/constants/urls';
 import { HttpOptions } from '../shared/models/httpOptions';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
 	providedIn: 'root'
@@ -11,22 +12,46 @@ import { HttpOptions } from '../shared/models/httpOptions';
 export class MenùService {
 	private httpOptions = new HttpOptions();
 
-	constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient, private toastrService: ToastrService) { }
 	
 	GetMenuByKind(kind: Kind) : Observable<Menù[]>{
 		return this.http.get<Menù[]>(MENU_URL+'/'+kind,this.httpOptions);
 	}
 
 	addNewElement(menù: Menù) : Observable<Menù>{
-		return this.http.post<Menù>(ADD_MENU_URL,menù,this.httpOptions);
+		return this.http.post<Menù>(ADD_MENU_URL,menù,this.httpOptions).pipe(
+			tap({
+				next: (menu) => { 
+					this.toastrService.success('Menù successfully added');
+				},
+				error: (errorResponse) => {
+					this.toastrService.error('Add menù failed');
+				}
+			}));
 	}
 
 	updateElement(id:string,menù: Menù) : Observable<Menù>{
-		return this.http.patch<Menù>(MENU_URL+'/'+id+'/update',menù,this.httpOptions);
+		return this.http.patch<Menù>(MENU_URL+'/'+id+'/update',menù,this.httpOptions).pipe(
+			tap({
+				next: (menu) => { 
+					this.toastrService.success('Menù successfully updated');
+				},
+				error: (errorResponse) => {
+					this.toastrService.error('Update menù failed');
+				}
+			}));
 	}
 
 
 	deleteElement(menù: Menù){
-		return this.http.delete<Menù>(MENU_URL+'/'+menù._id+'/delete',this.httpOptions);	
+		return this.http.delete<Menù>(MENU_URL+'/'+menù._id+'/delete',this.httpOptions).pipe(
+			tap({
+				next: (menu) => { 
+					this.toastrService.success('Menù successfully deleted');
+				},
+				error: (errorResponse) => {
+					this.toastrService.error('Delete menù failed');
+				}
+			}));;	
 	}
 }

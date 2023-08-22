858d4483a837f5abaec53c98f0ce53caeb39cb2a
lustocan <matteolovatin@hotmail.com>
2023-08-22 15:32:47 +0200

    Fixed login

--- backend/src/router/users.ts
+++ backend/src/router/users.ts
@@ -8,5 +8,5 @@ export default (router : express.Router) => {
     router.get('/users' , isAuthenticated, getAllUsers);
     router.delete('/users/:id', isAuthenticated, isAdmin , deleteUser);
     router.patch('/users/:id', isAuthenticated, isAdmin , updateUser);
-    router.get('/user' , getUser);
+    router.get('/user' , isAuthenticated ,getUser);
 }
